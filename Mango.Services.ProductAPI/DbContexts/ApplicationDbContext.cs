e677e2d17f2a4463c66728e3a817eb67410336f1
Jayasree Prakasan <jayasree.prakasan1@outlook.com>
2022-09-22 16:57:46 +0530

    Add project files.

---
+++ Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext:DbContext
+    {
+        //DbContext is from Microsoft.EntityFrameworkCore, DbContetxts is a folder we created
+        //ctor+Tab twice => to create constructor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+    }
+}
---
+++ Mango.Services.ProductAPI/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.ProductAPI.Models
+{
+    public class Product
+    {
+        //prop+Tab twice to get code for property
+
+        //Key is a data annotation to mark the product as primary key
+        [Key]
+        public int ProductId { get; set; }
+        
+        [Required] //to make sure in DB its not a nullable filed
+        public string ProductName { get; set; }
+        
+        [Range(1,1000)]
+        public double Price { get; set; }
+
+    }
+}
